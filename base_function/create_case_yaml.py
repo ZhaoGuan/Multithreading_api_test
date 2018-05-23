d85851190a6b03d9a6ac7589f8118bf4d1a7b924
ZhaoGuan <ZhaoGuan@users.noreply.github.com>
2018-05-23 16:46:07 +0800

    add source web0

--- base_function/create_case_yaml.py
+++ base_function/create_case_yaml.py
@@ -28,6 +28,7 @@ def create_case_list(folder_name, source='online'):
 
 if __name__ == '__main__':
     # case_list('backend-content-sending')
-    create_case_list('backend-content-sending', 'online')
+    # create_case_list('backend-content-sending', 'online')
+    create_case_list('backend-content-sending', 'web0')
     # create_case_list('gifsearch', 'online')
     # create_case_list('backend-picture', 'online')
