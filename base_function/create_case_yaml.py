2e5730dffc28319a08e101ea59bf284c86e6f734
ZhaoGuan <ZhaoGuan@users.noreply.github.com>
2018-06-13 16:02:54 +0800

    save

--- base_function/create_case_yaml.py
+++ base_function/create_case_yaml.py
@@ -21,7 +21,10 @@ def create_case_list(folder_name, source='online'):
     case_result = {}
     caselist = case_list(folder_name)
     for case in caselist:
-        case_result.update({case: {'path': case, 'source': source}})
+        if '!' in case:
+            pass
+        else:
+            case_result.update({case: {'path': case, 'source': source}})
     with open(PATH + '/../temp/cases.yaml', 'w') as case:
         yaml.dump(case_result, case, default_flow_style=False)
 
@@ -29,8 +32,8 @@ def create_case_list(folder_name, source='online'):
 if __name__ == '__main__':
     # case_list('backend-content-sending')
     # create_case_list('backend-content-sending', 'online')
-    # create_case_list('backend-content-sending', 'web0')
+    create_case_list('backend-content-sending', 'web0')
     # create_case_list('backend-content-sending', 'test')
     # create_case_list('gifsearch', 'online')
     # create_case_list('backend-picture', 'online')
-    create_case_list('ip_group', 'test')
+    # create_case_list('ip_group', 'test')
