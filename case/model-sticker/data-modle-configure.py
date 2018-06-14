a685891a098acc45c793fa34470fb0ae25e5929a
ZhaoGuan <ZhaoGuan@users.noreply.github.com>
2018-06-14 12:07:50 +0800

    修改modlesticker用例

--- case/model-sticker/data-modle-configure.py
+++ case/model-sticker/data-modle-configure.py
@@ -153,6 +153,7 @@ def case_runner(test_case, url):
                 print(test_case)
                 print(response)
                 print('bucketName错误 ' + '预期为 ' + test_case['result']['bucketName'])
+                print('失败')
     except Exception as e:
         print(e)
         print('失败')
