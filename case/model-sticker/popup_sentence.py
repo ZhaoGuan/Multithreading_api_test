cd6a79246caefab59f603b1a3161c78daea2a80f
ZhaoGuan <ZhaoGuan@users.noreply.github.com>
2018-07-13 19:38:16 +0800

    save

--- case/model-sticker/popup_sentence.py
+++ case/model-sticker/popup_sentence.py
@@ -13,10 +13,10 @@ sys.path.append(PATH + '/../../')
 from base_function.golable_function import source_input
 from base_function.Inspection_method import Inspection_method
 
-config = {'errorCode': 'Int', 'limitScore': 'Int',
+config = {'errorCode': 'Int', 'limitScore': '@@@',
           'extra': {'bucketName': 'Str', 'language': 'Str', 'tag': 'Str', 'management': 'Str', 'product': 'Str',
                     'popup_mode': 'Str', 'source': 'Str', 'param': 'Str', 'sessionId': 'Str', 'taghit': 'Str',
-                    'sentence': 'Str', 'scenario': 'Str'}, 'model': 'Str', 'md5': 'Str', 'score': 'Int',
+                    'sentence': 'Str', 'scenario': 'Str'}, 'model': 'Str', 'md5': 'Str', 'score': '@@@',
           'errorMsg': 'Str'}
 
 
